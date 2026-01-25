69341ed21d5c4ebdcd8f03605cf3c81dc75f0e4e
Utkarsh Naman <truly.away@gmail.com>
2026-01-25 20:55:27 +0530

    made time dynamic

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,16 +1,27 @@
+import { useEffect, useState } from "react";
 import dayjs from "dayjs";
 
-import {navIcons, navLinks} from "#constants/index.js";
+import { navIcons, navLinks } from "#constants/index.js";
 
 const Navbar = () => {
+    const [time, setTime] = useState(dayjs());
+
+    useEffect(() => {
+        const interval = setInterval(() => {
+            setTime(dayjs());
+        }, 1000); // update every second
+
+        return () => clearInterval(interval); // cleanup on unmount
+    }, []);
+
     return (
         <nav>
             <div>
-                <img src={"/images/logo.svg"} alt="logo" />
-                <p className={"font-normal"}>Utkarsh Naman</p>
+                <img src="/images/logo.svg" alt="logo" />
+                <p className="font-normal">Utkarsh Naman</p>
 
                 <ul>
-                    {navLinks.map(({id, name}) =>(
+                    {navLinks.map(({ id, name }) => (
                         <li key={id}>
                             <p>{name}</p>
                         </li>
@@ -20,16 +31,21 @@ const Navbar = () => {
 
             <div>
                 <ul>
-                    {navIcons.map(({id, img}) =>(
+                    {navIcons.map(({ id, img }) => (
                         <li key={id}>
-                            <img src={img} alt={`Navbar-Icon${id}`} className={"icon-hover"}/>
+                            <img
+                                src={img}
+                                alt={`Navbar-Icon${id}`}
+                                className="icon-hover"
+                            />
                         </li>
                     ))}
                 </ul>
 
-                <time>{dayjs().format("ddd MMM D h:mm A")}</time>
+                <time>{time.format("ddd MMM D h:mm A")}</time>
             </div>
         </nav>
-    )
-}
-export default Navbar
+    );
+};
+
+export default Navbar;
