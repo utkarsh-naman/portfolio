82f562e3c002b4f3a97de1d63aeca5ee40ebfefb
Utkarsh Naman <truly.away@gmail.com>
2026-01-29 16:34:06 +0530

    poll interval time = 1 min

--- src/components/NotchNook/NotchNook.jsx
+++ src/components/NotchNook/NotchNook.jsx
@@ -411,7 +411,7 @@ import NotificationPanel from "./NotificationPanel";
 import "./notchNook.css";
 
 const API_URL = "https://utkarsh-naman.github.io/repo-updates/get-updates.json";
-const POLL_INTERVAL = 15000;
+const POLL_INTERVAL = 60000;
 
 export default function NotchNook() {
     // UI States
